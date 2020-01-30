054f49f47fcdb675c27eb0ef356864fcdc4fda83
Pablo Aizpiri <pablo.aizpiri@gmail.com>
2020-01-29 19:49:30 -0600

    Bug fixes and basic graphics (triangle)

--- file_utils.cpp
+++ file_utils.cpp
@@ -1,6 +1,6 @@
 #include "file_utils.hpp"
 
-std::string FileUtils::ReadStringFromFile(const char * file_path) {
+std::string FileUtils::ReadString(const char * file_path) {
 	std::ifstream fileStream (file_path, std::ios::in);
 	std::string contents;
 	if(fileStream.is_open()) {
@@ -12,4 +12,8 @@ std::string FileUtils::ReadStringFromFile(const char * file_path) {
 		printf("Could not read file '%s'.\n", file_path);
 	}
 	return contents;
+}
+
+char const * FileUtils::ReadStringToPointer(const char * file_path) {
+	return FileUtils::ReadString(file_path).c_str();
 }
