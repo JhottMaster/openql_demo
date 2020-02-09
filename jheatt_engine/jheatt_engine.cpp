13863dc555fc3763c5c03c144e58fee557980967
Pablo Aizpiri <pablo.aizpiri@gmail.com>
2020-02-08 18:05:10 -0600

    Multiple cube render

--- jheatt_engine/jheatt_engine.cpp
+++ jheatt_engine/jheatt_engine.cpp
@@ -52,8 +52,8 @@ int main() {
     float timeValue, sineWavValue;
 
     // Model matrix:
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
+    // glm::mat4 model = glm::mat4(1.0f);
+    // model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
 
     // View matrix:
     glm::mat4 view = glm::mat4(1.0f);
@@ -64,7 +64,24 @@ int main() {
     glm::mat4 projection;
     projection = glm::perspective(glm::radians(45.0f), 1024.0f / 768.0f, 0.1f, 100.0f);
 
+    // Transaction matrix:
+    mat4 trans = mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+
     glEnable(GL_DEPTH_TEST);
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
 
     while (windowShouldStayOpen()) {
         window.clearCurrentBuffer();
@@ -78,22 +95,27 @@ int main() {
         simpleShader.SetIntVariable("tex", 0); // or with shader class
         simpleShader.SetIntVariable("tex2", 1); // or with shader class
 
-        mat4 trans = mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-        //trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
-        model = glm::rotate(model, sineWavValue * glm::radians(1.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
-        int modelLoc = glGetUniformLocation(simpleShader.shaderProgram, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
         int viewLoc = glGetUniformLocation(simpleShader.shaderProgram, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
         int projectionLoc = glGetUniformLocation(simpleShader.shaderProgram, "projection");
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-                
-        //glDrawElements(GL_TRIANGLES, plane.number_of_indexes, GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+
+            float angle = 50.0f * (i+1) * sineWavValue; 
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+            int modelLoc = glGetUniformLocation(simpleShader.shaderProgram, "model");
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+                    
+            //glDrawElements(GL_TRIANGLES, plane.number_of_indexes, GL_UNSIGNED_INT, 0);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        
 
         window.swapBuffersAndCheckForEvents();
     }
